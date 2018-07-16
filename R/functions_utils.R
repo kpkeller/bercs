bc05b6d648f09982b3f8ae37a2df05c24f0534e4
jpkeller <jkelle46@jhu.edu>
2018-07-16 14:29:46 -0400

    updated description

--- R/functions_utils.R
+++ R/functions_utils.R
@@ -1,7 +1,7 @@
 ##' @title Create spline matrix
 ##' @description Wrapper function for creating a spline matrix
 ##' @param x Variable values for which spline is to be created.
-##' @param df Degrees of freedom for spline. If set to 1, then a linear function assumed; otherwise \code{df} is passed to \code{fn}. 
+##' @param df Degrees of freedom for spline. If set to 1, then a linear function assumed; otherwise \code{df} is passed to \code{fn}.
 ##' @param fn Character string specifying the spline-generating function. Defaults to \code{\link[splines]{ns}}.
 ##' @param ... additional arguments passed to \code{fn}.
 ##' @export
@@ -13,7 +13,7 @@ create_spline_matrix <- function(x, df=1, fn="ns",...){
         fn <- get(fn)
         Hx <- fn(x=x,
                  df=df,
-                 ...)    
+                 ...)
     }
     Hx
 }
@@ -40,7 +40,7 @@ create_spline_range <- function(df, xseq=NULL, xrange=NULL, by=1, fn="ns", ...){
         if (is.null(xrange)){
             stop("Either 'xseq' or 'xrange' required.")
         } else {
-            xseq <- seq(xrange[1], to=xrange[2], by=by)    
+            xseq <- seq(xrange[1], to=xrange[2], by=by)
         }
     }
     out <- create_spline(xseq, df=df, fn=fn, ...)
@@ -55,28 +55,28 @@ create_spline_range <- function(df, xseq=NULL, xrange=NULL, by=1, fn="ns", ...){
 ##' @importFrom utils getS3method
 predict_spline <- function(spline, newx, center=FALSE, scale=FALSE){
     spline_class <- class(spline)
-    if(spline_class[1]=="matrix") { 
+    if(spline_class[1]=="matrix" && length(spline_class)==1) {
         # NOT checking 'inherits' here, since most splines are matrices, but not vice versa
-        # THen odf = 1 
+        # THen odf = 1
         if (ncol(spline)!=1) stop("Error: spline has >1 column but is class matrix only")
         H <- as.matrix(newx)
     } else {
         predfn <- utils::getS3method(f="predict", class(spline))
-        H <- predfn(spline, newx=newx)    
+        H <- predfn(spline, newx=newx)
     }
-    
+
     if(any(as.logical(center)) || any(as.logical(scale))){
-        H <- scale(H, center=center,scale=scale)    
+        H <- scale(H, center=center,scale=scale)
     }
     H
 }
 
 
 ##' @title Add time spline to a standata object
-##' @description Updates a standata object with a matrix of time or exposure splines 
+##' @description Updates a standata object with a matrix of time or exposure splines
 ##' @param standata the standata object to be updated
 ##' @param Ht,Hx the matrix of time or exposure splines to be added
-##' @details \code{add_Ht_standata} updates the \code{Ht}, \code{timedf}, and \code{Ht_attr} elements of \code{standata}. \code{add_Hx_standata} updates the \code{Hx}, \code{xdf}, and \code{Hx_attr} elements of \code{standata}. 
+##' @details \code{add_Ht_standata} updates the \code{Ht}, \code{timedf}, and \code{Ht_attr} elements of \code{standata}. \code{add_Hx_standata} updates the \code{Hx}, \code{xdf}, and \code{Hx_attr} elements of \code{standata}.
 ##' @seealso \code{\link{create_standata_exposure}}, \code{\link{create_standata_outcome}}, \code{\link{expsim_update_parameter}}, \code{\link{outsim_update_parameter}}, \code{\link{create_spline_range}}
 ##' @export
 add_Ht_standata <- function(standata, Ht){
@@ -117,7 +117,7 @@ check_names <- function(df, expected_names){
     if(any(!present)){
         pp <- expected_names[!present]
         for (p in 1:length(pp)){
-            stop(paste0("Column `", pp[p], "' required but not present."))    
+            stop(paste0("Column `", pp[p], "' required but not present."))
         }
     }
 }
@@ -127,7 +127,7 @@ check_names_to_overwrite <- function(df, expected_names){
     if(any(present)){
         pp <- expected_names[present]
         for (p in 1:length(pp)){
-            warning(paste0("Column `", pp[p], "' will be modified"))    
+            warning(paste0("Column `", pp[p], "' will be modified"))
         }
     }
 }
