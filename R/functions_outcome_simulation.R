b32ae9e8012f5242127c3745d2f5be105e54a449
jpkeller <joshua.keller@colostate.edu>
2018-11-08 17:30:43 -0700

    update argument names for outsim

--- R/functions_outcome_simulation.R
+++ R/functions_outcome_simulation.R
@@ -6,10 +6,10 @@
 ##' @param design String indicating trial design. Currently only 'parallel' is supported.
 ##' @param ... Additional arguments passed to the design-specific functions.
 ##' @details These functions create the blank structure of groups, clusters, households, and subject.
-##' Once parameters are set via \code{\link{expsim_update_parameter}} and related functions, then data can be sampled via 
+##' Once parameters are set via \code{\link{expsim_update_parameter}} and related functions, then data can be sampled via
 ##' \code{\link{expsim_sample_observations}}
 ##' and posterior parameter estimates obtained from \code{\link{sample_exposure_model}}.
-##' 
+##'
 ##' For creating a \code{standata_outcome} object from an existing 'wide' format data frame, see \code{\link{create_standata_outcome}}.
 ##' @return A list containing two sublists: \code{structure}, which contains settings and parameters for generating data, and \code{standata} which contains the study data in a format for sampling via STAN.
 #' @family outcome simulation functions
@@ -35,7 +35,7 @@ create_outcome_simulation_skeleton <- function(design="parallel",...){
 ##' @param nstudies Number of studies.
 ##' @param nclusters Number of clusters per study. If not of length \code{nstudies}, the same value will be used for all studies.
 ##' @param nsubjects Number of subjects.
-##' @param nobs Number of observations per subject. If length 1, value is repeated for all subjects. If length \code{sum(nclusters)}, then each element is repeated for all subjects within the corresponding cluster. 
+##' @param nobs Number of observations per subject. If length 1, value is repeated for all subjects. If length \code{sum(nclusters)}, then each element is repeated for all subjects within the corresponding cluster.
 ##' @param study_of_cluster optional vector of positive integers that provide the study number of each cluster. Should have length \code{sum(nclusters)}.
 ##' @param cluster_of_subj optional vector that provides the cluster number of each subject. Should have length \code{sum(nsubjects)}.
 ##' @param subj_of_obs optional vector that provides the subject number of each observation. Should have length \code{sum(nobs)}.
@@ -43,7 +43,7 @@ create_outcome_simulation_skeleton <- function(design="parallel",...){
 ##' @param x Exposure concentration values. Defaults to 0 and can be updated later via \code{\link{outsim_update_covariate}}.
 ##' @param nT optional time-at-risk value. See \code{\link{outsim_update_atrisk}}
 ##' @param times optional times for observations. Can be set later via \code{\link{sim_update_times}}.
-##' @param xfn Exposure-response function. Can be set later via \code{\link{outsim_update_parameter}}. 
+##' @param xfn Exposure-response function. Can be set later via \code{\link{outsim_update_parameter}}.
 ##' @param timefn Function relating time to mean outcome. Can be set later via \code{\link{outsim_update_parameter}}.
 ##' @param verbose should messages be printed.
 ##' @export
@@ -65,7 +65,7 @@ create_outcome_simulation_skeleton_parallel <- function(nstudies=1,
     if(!is.null(nclusters) && !check_all_nonnegative_value(nclusters)) stop("'nclusters' should be a non-negative integer and must have at least one positive element.")
     if(!check_all_nonnegative_value(nsubjects)) stop("'nsubjects' should be a non-negative integer and must have at least one positive element.")
     if(!check_all_postive_value(nstudies)) stop("'nstudies' should be a positive integer.")
-    
+
     # Process nclusters
     if(!length(nclusters) %in% c(1, nstudies)) stop("'nclusters' must have length 1 or 'nstudies'.")
     if (length(nclusters)==1) {
@@ -75,7 +75,7 @@ create_outcome_simulation_skeleton_parallel <- function(nstudies=1,
         }
     }
     K <- sum(nclusters)
-    
+
     # Process nsubjects
     if(!length(nsubjects) %in% c(1, K)) stop("'nsubjects' must have length 1 or 'sum(nclusters)'.")
     if (length(nsubjects)==1) {
@@ -85,7 +85,7 @@ create_outcome_simulation_skeleton_parallel <- function(nstudies=1,
         }
     }
     n <- sum(nsubjects)
-    
+
     # Process nobs
     if(!length(nobs) %in% c(1, K, n)) stop("'nobs' must have length 1, 'sum(nclusters)', or 'sum(nsubjects)'.")
     if (length(nobs)==1) {
@@ -100,7 +100,7 @@ create_outcome_simulation_skeleton_parallel <- function(nstudies=1,
         }
     }
     N <- sum(nobs)
-    
+
     # Defaults
     if (missing(study_of_cluster)) study_of_cluster <- rep(1:nstudies, times=nclusters)
     if (missing(cluster_of_subj)) cluster_of_subj <- rep(1:K, times=nsubjects)
@@ -108,13 +108,13 @@ create_outcome_simulation_skeleton_parallel <- function(nstudies=1,
     cluster_of_obs <- cluster_of_subj[subj_of_obs]
     study_of_subj <- study_of_cluster[cluster_of_subj]
     study_of_obs <- study_of_cluster[cluster_of_obs]
-    
+
     # Defaults
     if (is.na(nT)) nT <- rep(1, N)
     if (length(x)==1) x <- rep(x, N)
     if (length(times)==1) times <- rep(times, N)
     if (length(beta0)==1) beta0 <- rep(beta0, nstudies)
-    
+
     study_structure <- list(nstudies=nstudies,
                             nclusters=nclusters,
                             nsubjects=nsubjects,
@@ -154,13 +154,13 @@ create_outcome_simulation_skeleton_parallel <- function(nstudies=1,
                            study_of_subj=study_of_subj,
                            study_of_obs=study_of_obs,
                            cluster_of_obs=cluster_of_obs)
-    
+
     class(study_standata) <- "standata_outcome"
-    
+
     obj <- list(structure=study_structure,
                 standata=study_standata)
     class(obj) <- "outsim"
-    
+
     obj <- outsim_update_logitmean(obj)
     obj
 }
@@ -202,8 +202,8 @@ outsim_update_logitmean <- function(obj){
 #' @param param character string giving name of parameter to update. If not provided, parameter name is taken from \code{level} and \code{type}
 #' @param level character string of model level being updated. See details.
 #' @param type character string giving type of parameter being updated. See details.
-#' @param val value(s) to which parameter should be set
-#' @param draw if \code{TRUE}, then the parameter value is sampled instead of being set to \code{val}
+#' @param value value(s) to which parameter should be set
+#' @param draw if \code{TRUE}, then the parameter value is sampled instead of being set to \code{value}
 #' @details Only specific combinations of \code{level} and \code{type} are allowed. They are:
 #' \itemize{
 #' \item \code{"group"}: \code{"mean"} and \code{"sd"} set 'etaG' and 'sigG', respectively
@@ -212,7 +212,7 @@ outsim_update_logitmean <- function(obj){
 #' @family outcome simulation functions
 #' @seealso \code{\link{expsim_update_parameter}}
 ##' @export
-outsim_update_parameter <- function(obj, param, level=c("study", "subject", "time", "covariate", "exposure"), type=c("mean", "sd", "re", "coef"), val=NULL, draw=is.null(val)){
+outsim_update_parameter <- function(obj, param, level=c("study", "subject", "time", "covariate", "exposure"), type=c("mean", "sd", "re", "coef"), value=NULL, draw=is.null(value)){
     if (!inherits(obj, c("outsim"))) stop("'obj' must be of class 'outsim'.")
 
     if (!missing(param)){
@@ -232,16 +232,16 @@ outsim_update_parameter <- function(obj, param, level=c("study", "subject", "tim
                         NA)
         if (is.na(param)) stop(paste0(level, "_", type, " is not a supported parameter."))
     }
-    
+
     # Need to do some error/length checking.
     current_length <- length(obj$structure[[param]])
     if (draw){
-        if(!is.null(val)) warning("'val' provided but 'draw=TRUE'. Ignoring provided value of 'val'.")
-        
+        if(!is.null(value)) warning("'value' provided but 'draw=TRUE'. Ignoring provided value of 'value'.")
+
         if (!param %in% c("reI")) {
             stop(paste0(param, " should be set, not drawn from a distribution."))
         }
-        
+
         # Need to check that mean/sd has been set, first.
         cur_mean <- switch(param,
                            reI=0)
@@ -249,17 +249,17 @@ outsim_update_parameter <- function(obj, param, level=c("study", "subject", "tim
                          reI=obj$structure$sigI)
         cur_n <- switch(param,
                         reI=obj$standata$n)
-        val <- rnorm(cur_n, cur_mean, cur_sd)
+        value <- rnorm(cur_n, cur_mean, cur_sd)
     }
-    if (length(val)!=current_length) {
-        if (length(val)==1){
-            val <- rep(val, current_length)
+    if (length(value)!=current_length) {
+        if (length(value)==1){
+            value <- rep(value, current_length)
         } else {
-            stop(paste0("'val' should have length ", current_length, "."))
+            stop(paste0("'value' should have length ", current_length, "."))
         }
     }
-    obj$structure[[param]] <- val
-    
+    obj$structure[[param]] <- value
+
     obj
 }
 
@@ -271,7 +271,7 @@ outsim_update_atrisk <- function(obj, nT=1){
     if (!inherits(obj, c("outsim"))) stop("'obj' must be of class 'outsim'.")
     if (length(nT)==1) nT <- rep(nT, obj$N)
     if (length(nT)!=obj$N) stop("Incorrect length for nT.")
-    obj$nT <- nT    
+    obj$nT <- nT
     obj
 }
 
@@ -287,21 +287,21 @@ outsim_update_atrisk <- function(obj, nT=1){
 ##' @param sd SD of normal dsitirbtion from which to draw.
 ##' @export
 ##' @importFrom stats rnorm
-outsim_update_covariate <- function(obj, covariate=c("x","Z"), ncol=1, val=NULL, draw=is.null(val), verbose=TRUE, mean=0, sd=1){
+outsim_update_covariate <- function(obj, covariate=c("x","Z"), ncol=1, value=NULL, draw=is.null(value), verbose=TRUE, mean=0, sd=1){
     covariate <- match.arg(covariate)
     if (covariate=="x" && ncol!=1){
         stop("'ncol' must equal 1 when 'covariate' is 'x'.")
     }
     if (draw){
-        if(!is.null(val)) warning("'val' is provided, but 'draw=TRUE'. Ignoring provided value of 'val'.")
+        if(!is.null(value)) warning("'value' is provided, but 'draw=TRUE'. Ignoring provided value of 'value'.")
 
         if (verbose){
             message(paste0("Drawing N(", mean, ",", sd, ") sample for covariate ", covariate, "."))
         }
         valdim <- c(obj$standata$N, ncol)
-        val <- matrix(stats::rnorm(valdim[1]*valdim[2], mean=mean, sd=sd), nrow=valdim[1], ncol=valdim[2])
+        value <- matrix(stats::rnorm(valdim[1]*valdim[2], mean=mean, sd=sd), nrow=valdim[1], ncol=valdim[2])
     } else {
-        valdim <- ifelse(is.vector(val), c(length(val), 1), dim(val))
+        valdim <- ifelse(is.vector(value), c(length(value), 1), dim(value))
     }
     covariate_param <- switch(covariate,
                               Z="gamma",
@@ -314,56 +314,56 @@ outsim_update_covariate <- function(obj, covariate=c("x","Z"), ncol=1, val=NULL,
             if (draw){
                 warning(paste0("Truncating model parameter ",covariate_param, " to the first ncol=", ncol, " elements."))
             } else {
-                warning(paste0("Truncating model parameter ",covariate_param, " to the first ncol(val)=", valdim[2], " elements."))
+                warning(paste0("Truncating model parameter ",covariate_param, " to the first ncol(value)=", valdim[2], " elements."))
             }
         }
     }
     if (covariate=="Z") obj$standata$p <- valdim[2]
-    
-    obj$structure[[covariate]] <- val
-    obj$standata[[covariate]] <- val
+
+    obj$structure[[covariate]] <- value
+    obj$standata[[covariate]] <- value
     obj
 }
 
 ##' @rdname outsim_update_parameter
 ##' @export
-outsim_update_set_parameter <- function(obj, param=c("beta", "gamma"), val=NULL){
+outsim_update_set_parameter <- function(obj, param=c("beta", "gamma"), value=NULL){
     param <- match.arg(param)
-    
-    if (is.null(val)){
-        stop("'val' must be provided.")
+
+    if (is.null(value)){
+        stop("'value' must be provided.")
     }
-    
+
     # Matrix corresponding to this parameer
-    param_mx <- switch(param, 
+    param_mx <- switch(param,
                        beta="X",
                        gamma="Z",
                        beta0=NA)
-    
-    # Length of val should match dimensions of corresponding matrix
-    # Repeat val if needed
+
+    # Length of value should match dimensions of corresponding matrix
+    # Repeat value if needed
     if (is.na(param_mx)){
-        # if param=beta0, then val should have length 1.
+        # if param=beta0, then value should have length 1.
         val_length <- 1
     } else if (!is.null(obj$standata[[param_mx]])) {
-        # if matrix exists, val should have length 1, or number of columns of matrix
+        # if matrix exists, value should have length 1, or number of columns of matrix
         val_length <- ifelse(is.vector(obj$standata[[param_mx]]), 1, ncol(obj$standata[[param_mx]]))
     } else {
-        # If matrix not set, then val can be any length
-        val_length <- length(val)
+        # If matrix not set, then value can be any length
+        val_length <- length(value)
     }
-    if (length(val)!=val_length){
-        if (length(val)==1) {
-            val <- rep(val, val_length)
+    if (length(value)!=val_length){
+        if (length(value)==1) {
+            value <- rep(value, val_length)
         } else {
             if (!is.na(param_mx)){
-                stop(paste0("Incorrect length for 'val'. Should have length 1 or length equal to the number of columns of ", param_mx))    
+                stop(paste0("Incorrect length for 'value'. Should have length 1 or length equal to the number of columns of ", param_mx))
             } else {
-                stop(paste0("Incorrect length for 'val'.  Should have length 1."))
+                stop(paste0("Incorrect length for 'value'.  Should have length 1."))
             }
         }
     }
-    obj$structure[[param]] <- val
+    obj$structure[[param]] <- value
     obj
 }
 
